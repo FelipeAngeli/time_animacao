ba26c606362ef23bc8dc84e5b6484979bc59d494
felipeangeli <felipe.angeli@gmail.com>
2022-05-18 20:15:06 -0300

    commit

---
+++ lib/animated.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AnimatedTimeRecipeCustom extends StatefulWidget {
+  const AnimatedTimeRecipeCustom({Key? key}) : super(key: key);
+
+  @override
+  _CountdownPageState createState() => _CountdownPageState();
+}
+
+class _CountdownPageState extends State<AnimatedTimeRecipeCustom>
+    with TickerProviderStateMixin {
+  late AnimationController controller;
+
+  bool isPlaying = false;
+
+  String get countText {
+    Duration count = controller.duration! * controller.value;
+    return controller.isDismissed
+        ? '${controller.duration!.inHours}:${(controller.duration!.inMinutes % 60).toString().padLeft(2, '0')}:${(controller.duration!.inSeconds % 60).toString().padLeft(2, '0')}'
+        : '${count.inHours}:${(count.inMinutes % 60).toString().padLeft(2, '0')}:${(count.inSeconds % 60).toString().padLeft(2, '0')}';
+  }
+
+  double progress = 1.0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 60),
+    );
+
+    controller.addListener(() {
+      if (controller.isAnimating) {
+        setState(() {
+          progress = controller.value;
+        });
+      } else {
+        setState(() {
+          progress = 1.0;
+          isPlaying = false;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF1e1e1e),
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.red[100],
+                    color: Colors.red,
+                    value: progress,
+                    strokeWidth: 6,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    if (controller.isDismissed) {
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) => Container(
+                          height: 60,
+                          child: CupertinoTimerPicker(
+                            initialTimerDuration: controller.duration!,
+                            onTimerDurationChanged: (time) {
+                              setState(() {
+                                controller.duration = time;
+                              });
+                            },
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                  child: AnimatedBuilder(
+                    animation: controller,
+                    builder: (context, child) => Text(
+                      countText,
+                      style: const TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    if (controller.isAnimating) {
+                      controller.stop();
+                      setState(() {
+                        isPlaying = false;
+                      });
+                    } else {
+                      controller.reverse(
+                          from: controller.value == 0 ? 1.0 : controller.value);
+                      setState(() {
+                        isPlaying = true;
+                      });
+                    }
+                  },
+                  child: RoundButton(
+                    icon: isPlaying == true ? Icons.pause : Icons.play_arrow,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    controller.reset();
+                    setState(() {
+                      isPlaying = false;
+                    });
+                  },
+                  child: RoundButton(
+                    icon: Icons.stop,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class RoundButton extends StatelessWidget {
+  const RoundButton({
+    Key? key,
+    required this.icon,
+  }) : super(key: key);
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 5,
+      ),
+      child: CircleAvatar(
+        radius: 30,
+        child: Icon(
+          icon,
+          size: 36,
+        ),
+      ),
+    );
+  }
+}
